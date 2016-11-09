4260c1c0192418895574116c2b160a15833b2b2a
Yilun Gao <yilungao@gmail.com>
2016-11-09 14:28:21 -0800

    changed the regression algorithm to use equations from French paper

--- src/norming.py
+++ src/norming.py
@@ -5,10 +5,44 @@ import sys
 
 def norm(x_values, y_values):
     C = min(y_values)*.99
-    print C
     transformed_y = [math.log(y - C) for y in y_values]
     return numpy.polyfit(x_values, transformed_y, 1)
 
+# better regression method from stack overflow post
+def betterNorm(x_values, y_values):
+    # make the S sequence
+    # equation is y = Ae^(Bx) + C
+    S = [0]
+    for k in range(1,len(x_values)):
+        S.append(S[k-1]+ .5*(y_values[k]+y_values[k-1])*(x_values[k]-x_values[k-1]))
+
+
+    matrixA = numpy.matrix([ [sum([math.pow(x - x_values[0],2) for x in x_values]), 
+    sum([(x - x_values[0])*s for x,s in zip(x_values,S)])],
+     [sum([(x - x_values[0])*s for x,s in zip(x_values,S)]), 
+     sum([math.pow(s,2) for s in S])]])
+
+    matrixB = numpy.matrix([[sum([(y - y_values[0])*(x - x_values[0]) for y,x in zip(y_values,x_values)])],
+    [sum([(y - y_values[0])*s for y,s in zip(y_values,S)])]])
+
+    matrixC = matrixA.I*matrixB
+    B_value = matrixC.item(1)
+
+    matrixD = numpy.matrix([[len(x_values),
+    sum([math.exp(B_value*x) for x in x_values])],
+    [sum([math.exp(B_value*x) for x in x_values]),
+    sum([math.exp(2*B_value*x) for x in x_values])]])
+
+    matrixE = numpy.matrix([[sum([y for y in y_values])],
+    [sum([y*math.exp(B_value*x) for y,x in zip(y_values,x_values)])]])
+
+    matrixF = matrixD.I*matrixE
+
+    C_value = matrixF.item(0)
+    A_value = matrixF.item(1)
+
+    return A_value, B_value, C_value
+    
 
 def function(x,A,B,C):
 
@@ -47,24 +81,23 @@ def main():
 
     shellWidths = [map(float,line.split(",")[1:]) for line in lines]
 
-    print shellWidths
-
     averageWidths = average(shellWidths)
 
     y_values = averageWidths
-    print y_values
+
     x_values = range(len(averageWidths))
 
-    [A,B] = norm(x_values,y_values)
-    C = min(y_values) *.99
+    [A,B,C] = betterNorm(x_values,y_values)
+
+    print A,B,C
 
     domain = numpy.arange(0,max(x_values),.1)
-    output = [function(x,math.exp(A),B,C) for x in domain]
+    output = [function(x,A,B,C) for x in domain]
 
     plt.plot(x_values,y_values, 'ro', domain, output)
     plt.show()
 
-    print math.exp(A),B,C
+    
 
 
 main()
